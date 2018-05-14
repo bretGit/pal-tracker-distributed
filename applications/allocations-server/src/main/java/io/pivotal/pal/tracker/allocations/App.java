a6ca30ecccb8c17ea0d5ed13a4339d61ea8b7ff1
pal_user <bret.mcfarland@wellsfargo.com>
2018-05-14 15:00:14 -0600

    finish circuit breaker

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -1,6 +1,5 @@
 package io.pivotal.pal.tracker.allocations;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
